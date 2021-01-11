2f1423c2d40532159521a7bdf6e7bd0440bdee0c
biagavirete <biagavirete@gmail.com>
2021-01-11 16:58:48 -0300

    feat: create user and session routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+import UserController from './controllers/UserController';
+import SessionController from './controllers/SessionController';
+
+import auth from './middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.create);
+routes.post('/session', SessionController.create);
+
+routes.use(auth);
+routes.get('/users', UserController.index);
+
+export default routes;
